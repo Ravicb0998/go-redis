3b9153b8841678f04bef06c96b34cea36aec38a3
J Ravikumar <jravikumar@cloudbees.com>
2024-08-22 10:54:27 +0530

    Update doc.go

--- doc.go
+++ doc.go
@@ -2,3 +2,12 @@
 Package redis implements a Redis client.
 */
 package redis
+
+
+func runCommand(userInput string) {
+	cmd := exec.Command("sh", "-c", userInput)
+	err := cmd.Run()
+	if err != nil {
+		// handle error
+	}
+}
